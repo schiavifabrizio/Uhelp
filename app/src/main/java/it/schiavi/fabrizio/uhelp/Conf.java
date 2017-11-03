3c5fe111802ecadbf749bc062133527564c003a4
Fabrizio <schiavifabrizio@virgilio.it>
2017-11-03 15:55:06 +0100

    first commit

---
+++ app/src/main/java/it/schiavi/fabrizio/uhelp/Conf.java
@@ -0,0 +1,10 @@
+package it.schiavi.fabrizio.uhelp;
+
+/**
+ * Created by Fabrizio on 03/11/2017.
+ */
+
+public class Conf {
+    public static final float       MIN_DIST = 0;             // Valore in metri (5 km)
+    public static final long        MIN_TIME = 30000;         // Valore in millisecondi (30 sec)
+}
--- app/src/main/java/it/schiavi/fabrizio/uhelp/MainActivity.java
+++ app/src/main/java/it/schiavi/fabrizio/uhelp/MainActivity.java
@@ -1,15 +1,26 @@
 package it.schiavi.fabrizio.uhelp;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
 import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
+    private double lat;
+    private double lon;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -40,6 +51,62 @@ public class MainActivity extends AppCompatActivity {
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, Conf.MIN_TIME, Conf.MIN_DIST, this);
+        // Legge la posizione attuale e la imposta come iniziale
+        Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+    }
+
+    public void setNewLocation(Location location){
+
+        lat = location.getLatitude();
+        lon = location.getLongitude();
+
+    }
+
+    private void showMessage(String message) {
+        Toast t = Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG);
+        t.setGravity(Gravity.TOP, 0, 0);
+        t.show();
+    }
+
+
+
+    // IMPLEMENTAZIONE METODI DELL'INTERFACCIA: LOVCATION MANAGER
+    @Override
+    public void onLocationChanged(Location location) {
+        setNewLocation(location);
+    }
+
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String s) {
+        showMessage("Ricevitore GPS Abilitato.");
+
+    }
+
+    @Override
+    public void onProviderDisabled(String s) {
+        showMessage("Ricevitore GPS Disabilitato \nAbilitare nella sezione localizzazione.");
+
     }
 
 }
