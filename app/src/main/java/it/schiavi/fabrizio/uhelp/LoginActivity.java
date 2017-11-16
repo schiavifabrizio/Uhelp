d182aa0f1061ce3e4c84dcab30cfca1a5a4b58e7
Jacopo <jac99c@gmail.com>
2017-11-16 14:26:41 +0100

    Update

---
+++ app/src/main/java/it/schiavi/fabrizio/uhelp/LoginActivity.java
@@ -0,0 +1,29 @@
+package it.schiavi.fabrizio.uhelp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        TextView textView2 = (TextView) findViewById(R.id.textView2);
+        textView2.setOnClickListener(new View.OnClickListener(){
+
+        @Override
+        public void onClick (View v) {
+            Intent intent = new Intent(LoginActivity.this, SignupActivity.class);
+            startActivity(intent);
+        }
+    });
+
+    }
+}
